aab959d8b364b201b03f55ba4737246bb1264e97
Dean Dunn <dean@deandunn.co.uk>
2021-06-24 16:14:37 +0300

    added colour highlighting for install steps, added check for curl success/fail

--- aws-wsl2-dev.sh
+++ aws-wsl2-dev.sh
@@ -3,25 +3,37 @@
 # aws-cli, unzip, jq, pip, git, SSH for git, clone initial repo, cfn-lint, Node.js, cfn-diagram, AWS CDK
 # As of date 15/06/2021 Python 3.9 is not recommend, use Python 3.8 which is default installed version on Ubuntu 20.04 
 #
-# Git personal access token: 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
+# You will need a GitHub Access Token with admin:public_key permissions
 # Thanks to Peter Sellars (github.com/petersellars) for the code to automate GitHub SSH key generation.
 # https://gist.github.com/petersellars/c6fff3657d53d053a15e57862fc6f567
+#
+# Usage:
+# ./aws-wsl2-dev.sh <aws-access-key-id> <aws-secret-access-key> <aws-default-region> <aws-default-output-format>
+#
+# <aws-access-key-id> - Your AWS Access key ID.
+# <aws-secret-access-key> - Your AWS Secret access key.
+# <aws-default-region> - AWS region you wish to set as default. i.e. us-east-1 (see https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-regions-availability-zones.html#concepts-regions for full list).
+# <aws-default-output-format> - Default cli output format. Valid values are json, yaml, yaml-stream, text, table (see https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-output-format.html for more details).
+#
+#
+# Variables for AWS and Github credentials (store at your own risk!)
+# Will be set to command line arguments if entered (as described above)
 
+AWS_DEFAULTACCESSKEYID=$1
+AWS_DEFAULTSECRETACCESSKEY=$2
+AWS_DEFAULTREGION=$3
+AWS_DEFAULTOUTPUTFORMAT=$4
+GIT_TOKEN=$5
+GREEN_TEXT='\033[0;32m'
+RED_TEXT='\033[0;31m'
 
-# Variables for AWS and Github credentials (use at your own risk!)
-AWS_DEFAULTACCESSKEYID=""
-AWS_DEFAULTSECRETACCESSKEY=""
-AWS_DEFAULTREGION=""
-AWS_DEFAULTOUTPUTFORMAT="yaml"
-GIT_TOKEN=""
-
-# Update to latest version of WSL2, install unzip and build tools
-echo "Update/Upgrade Ubuntu..."
+# Update to latest version of Ubuntu/install unzip.
+echo -e "${GREEN_TEXT}Update/Upgrade Ubuntu..."
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt install unzip -y
 
 # Install and configure AWS CLI. Config file default location is ~/.aws/config
-echo "Install and configure AWS CLI..."
+echo -e "${GREEN_TEXT}Install and configure AWS CLI..."
 echo -n "Enter default Access Key ID, or press enter for default value ["$AWS_DEFAULTACCESSKEYID"]:"
 read AWS_INPUT_VARIABLE
 if [ -n "$AWS_INPUT_VARIABLE" ]
@@ -62,28 +74,22 @@ aws configure set aws_secret_access_key $AWS_DEFAULTSECRETACCESSKEY
 aws configure set default.region $AWS_DEFAULTREGION
 
 #Install JSON command line parser utility
-echo "Installing jq JSON parser..."
+echo -e "${GREEN_TEXT}Installing jq JSON parser..."
 sudo apt install jq -y
 
-#Install Python - Check if really need to use deadsnakes ppa
-# sudo apt install software-properties-common
-# sudo add-apt-repository ppa:deadsnakes/ppa
-# sudo apt update
-# sudo apt install python3.8
-
 # Install Package Installer for Python (PIP)
-echo "Installing PIP..."
+echo -e "${GREEN_TEXT}Installing Package Installer for Python(PIP)..."
 sudo apt install python3-pip -y
 
 # Install latest stable version of Git. Git is included with Ubuntu 20.04 distro but not latest version of Git.
-echo "Installing latest version of Git..."
+echo -e "${GREEN_TEXT}Installing latest version of Git..."
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update
 sudo apt install git -y
 
 # Setup SSH for Git. ssh-keygen will handle empty email/empty password conditions
 GIT_TOKEN="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"
-echo "SSH setup for GitHub"
+echo -e "${GREEN_TEXT}Install and configure SSH for GitHub"
 echo "Enter your email address (comment for SSH key)"
 read -p 'Email Address: ' SSH_EMAIL
 echo "When prompted, enter your passphrase. This will be used to authenticate every time you start a new WSL2 session."
@@ -96,15 +102,22 @@ TITLE=`hostname`-autogen
 RESPONSE=`curl -s -H "Authorization: token ${GIT_TOKEN}" \
   -X POST --data-binary "{\"title\":\"${TITLE}\",\"key\":\"${PUBKEY}\"}" \
   https://api.github.com/user/keys`
+  
+CURL_RESULT=$?
+if test "$CURL_RESULT" != "0"; then
+   echo -e"${RED_TEXT}curl command failed. Exit code: $CURL_RESULT. Script aborted."
+   exit 1
+fi
 KEYID=`echo $RESPONSE \
   | grep -o '\"id.*' \
   | grep -o "[0-9]*" \
   | grep -m 1 "[0-9]*"`
-echo "SSH public key added succesfully to GitHub account. KeyID - " $KEYID
+echo -e "${GREEN_TEXT}SSH public key added succesfully to GitHub account. KeyID: $KEYID"
 
 # Add key to ssh-agent, install keychain (manager for ssh-agent) and add startup to bash profile.
 eval "$(ssh-agent -s)"
-echo "Adding key to ssh-agent. Enter your passphrase."
+echo -e "${GREEN_TEXT}Adding ssh-key to ssh-agent. 
+echo "Enter your secret passphrase to authenticate."
 ssh-add ~/.ssh/github_ed25519
 echo "Installing keychain..."
 sudo apt install keychain -y
@@ -112,26 +125,17 @@ echo "Adding keychain to .profile for session autostart"
 echo '/usr/bin/keychain --nogui $HOME/.ssh/github_ed25519' >> ~/.profile
 echo 'source $HOME/.keychain/$HOSTNAME-sh' >> ~/.profile
 
-# Change remote URL to SSH
-#git remote set-url origin git@github.com:USERNAME/REPOSITORY.git
-
-# Show remote repositories
-#git remote -v
-
-# Clone GitHub repository
-#git clone git@github.com:deanflyer/aws-wordpress.git
-
 # Install CloudFormation linter via Brew instead of pip
 # pip3 install conflicts with AWS SAM, get following error
 # ERROR: aws-sam-translator 1.36.0 has requirement six~=1.15, but you'll have six 1.14.0 which is incompatible.
-echo "Installing cfn-lint..."
+echo -e"${GREEN_TEXT}Installing cfn-lint..."
 #pip3 install cfn-linter
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew install cfn-lint
 
 #Install Node.js
-echo "Installing Node.js..."
+echo -e "${GREEN_TEXT}Installing Node.js..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -141,12 +145,15 @@ nvm install node --lts
 nvm use node --lts
 
 #Install cfn-diagram
-echo "Installing cfn-diagram..."
+echo -e "${GREEN_TEXT}Installing cfn-diagram..."
 npm i -g @mhlabs/cfn-diagram
 
 #Install AWS CDK
-echo "Installing AWS CDK..."
+echo -e "${GREEN_TEXT}Installing AWS CDK..."
 npm -g install typescript
 npm install -g aws-cdk
+
+echo -e "${GREEN_TEXT}Install complete. Please exit and restart shell to complete changes."
+exit 0
 
 
